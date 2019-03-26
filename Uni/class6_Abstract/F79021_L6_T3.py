f40d4f930f6de4e942128cd9184dc86f2e26b377
booobi <qqwweee59@gmail.com>
2019-03-26 16:12:48 +0200

    Update palindrom and recursive binary search

--- Uni/class6_Abstract/F79021_L6_T3.py
+++ Uni/class6_Abstract/F79021_L6_T3.py
@@ -3,35 +3,18 @@ import sys
 
 inputList = [30, 40, 50, 52, 56, 62, 70, 91, 100, 131, 132, 166, 170, 195, 202, 205, 212, 248, 249, 256, 263, 272, 288, 289, 290, 296, 332, 345, 352, 364, 380, 390, 407, 412, 429, 430, 438, 444, 486, 493, 497, 499, 510, 513, 514, 519, 521, 521, 535, 546, 552, 554, 556, 570, 584, 638, 640, 655, 655, 657, 692, 692, 711, 713, 731, 739, 740, 842, 858, 885, 887, 888, 893, 898, 900, 903, 908, 909, 959, 988]
 
-# testList = [1,2,3,4,5]
 inputNumber = int(sys.argv[1])
 
-def recursiveBinarySearch(listToSearch, numberToSearch):
-    if listToSearch[len(listToSearch)/2] == numberToSearch:
-        print "found at", len(listToSearch)/2
-    elif numberToSearch < listToSearch[len(listToSearch)/2]:
-        recursiveBinarySearch(listToSearch[:len(listToSearch)/2], numberToSearch)
-    elif numberToSearch > listToSearch[len(listToSearch)/2]:
-        recursiveBinarySearch(listToSearch[len(listToSearch)/2:], numberToSearch)
+def recursiveBinarySearch(listToSearch, numberToSearch, startIndex, endIndex):    
+    if(startIndex < endIndex):
+        mid = startIndex + (endIndex - startIndex)/2
+        if listToSearch[mid] == numberToSearch:
+            return mid
+        elif numberToSearch < listToSearch[mid]:
+            return recursiveBinarySearch(listToSearch, numberToSearch, startIndex, mid) 
+        elif numberToSearch > listToSearch[mid]:
+            return  recursiveBinarySearch(listToSearch, numberToSearch, mid + 1, endIndex)
+    return -1
     
-recursiveBinarySearch(inputList,inputNumber)
-
-
-# [1,2,3,4] - looking for 3
-# found at 2
-
-# [1,2,3,4,5] - looking for 1
-# [1,2,3]
-# [1,2]
-# 
-
-# [1,2,3,4,5,6] - looking for 6
-# [4,5,6]
-# [5,6] - found at 1 
-
-# [1,2,3,4,5,6] - looking for 4
-# [4,5,6]
-# [5,6] - found at 1 
-
-# [1,2,3,4,5] - looking for 4
-# [3,4,5] - found at 1
+foundIndex = recursiveBinarySearch(inputList,inputNumber, 0, len(inputList))
+print "not found" if foundIndex ==-1 else ("found at", foundIndex)
--- Uni/class6_Abstract/F79021_L6_T4.py
+++ Uni/class6_Abstract/F79021_L6_T4.py
@@ -3,7 +3,7 @@ import sys
 inputWord = sys.argv[1].lower()
 
 def isPalindrom(word):
-    if len(word) == 1 or len(word) == 0:
+    if len(word) <= 1:
         return True
     else:
         return word[0] == word[-1] and isPalindrom(word[1:-1])
