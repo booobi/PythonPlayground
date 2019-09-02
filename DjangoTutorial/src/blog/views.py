82fd7ef2b2f233aee1fb1695c88d62fb9c657ee7
booobi <qqwweee59@gmail.com>
2019-09-02 10:45:58 +0300

    register articles in admin site, fix article list view

--- DjangoTutorial/src/blog/views.py
+++ DjangoTutorial/src/blog/views.py
@@ -12,3 +12,6 @@ from .models import Article
 class ArticleListView(ListView):
     template_name = "articles/article_list.html"
     queryset = Article.objects.all()
+
+class ArticleDetailView(DetailView):
+    pass
