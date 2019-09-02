89f48f8381246e95aea5c714e761a52e971d4588
booobi <qqwweee59@gmail.com>
2019-09-02 11:22:17 +0300

    add creade,update,delete class based views to articles

--- DjangoTutorial/src/blog/views.py
+++ DjangoTutorial/src/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
 from django.views.generic import (
     CreateView,
     DetailView,
@@ -8,6 +9,7 @@ from django.views.generic import (
 )
 
 from .models import Article
+from .forms import ArticleForm
 
 class ArticleListView(ListView):
     template_name = "articles/article_list.html"
@@ -16,6 +18,39 @@ class ArticleListView(ListView):
 class ArticleDetailView(DetailView):
     template_name = "articles/article_detail.html"
     queryset = Article.objects.all()
+
+    def get_object(self, queryset=None):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Article, id=id_)
+
+class ArticleCreateView(CreateView):
+    template_name = "articles/article_create.html"
+    form_class = ArticleForm
+    queryset = Article.objects.all()
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super().form_valid(form)
+
+class ArticleUpdateView(UpdateView):
+    template_name = "articles/article_create.html"
+    form_class = ArticleForm
+    queryset = Article.objects.all()
+
+    def get_object(self, queryset=None):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Article, id=id_)
+
+    def form_valid(self, form):
+        print(form.cleaned_data)
+        return super().form_valid(form)
+
+class ArticleDeleteView(DeleteView):
+    template_name = "articles/article_delete.html"
+    queryset = Article.objects.all()
+    
+    def get_success_url(self):
+        return reverse("articles:article-list")
 
     def get_object(self, queryset=None):
         id_ = self.kwargs.get("id")
