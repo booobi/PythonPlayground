580e721769d1186de7b387e6941244f31957388d
booobi <qqwweee59@gmail.com>
2019-09-02 10:55:16 +0300

    implement article detail view

--- DjangoTutorial/src/blog/views.py
+++ DjangoTutorial/src/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import (
     CreateView,
     DetailView,
@@ -14,4 +14,9 @@ class ArticleListView(ListView):
     queryset = Article.objects.all()
 
 class ArticleDetailView(DetailView):
-    pass
+    template_name = "articles/article_detail.html"
+    queryset = Article.objects.all()
+
+    def get_object(self, queryset=None):
+        id_ = self.kwargs.get("id")
+        return get_object_or_404(Article, id=id_)
